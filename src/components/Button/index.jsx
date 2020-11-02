4323b731236e1d4fca22ee7e0b6f8ca4258cd1ef
adeministratorTr <adembavas@gmail.com>
2020-11-02 21:03:29 +0300

    Add active property to button component and related style

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,9 @@
 import './style.scss'
 
 export const Button = ({ type, children, ...props }) => {
+  const btnClassName = props.active ? 'group__button--active' : 'group__button'
   const renderGroupButton = () => (
-    <button onClick={props.onClick} className="group__button">
+    <button onClick={props.onClick} className={btnClassName}>
       <p className="group__button__text">{props.text}</p>
       <p className="group__button__number">{props.length}</p>
     </button>
