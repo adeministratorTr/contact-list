67b67af7db3cd06024e12c9dd12fe9781a1fa5b2
adeministratorTr <adembavas@gmail.com>
2020-11-03 12:43:11 +0300

    Add more e2e tests

--- cypress/integration/contact.spec.js
+++ cypress/integration/contact.spec.js
@@ -5,4 +5,19 @@ describe('contact page e2e test', () => {
     cy.visit('/')
     cy.contains('Loading...')
   })
+  it('should render surname category buttons', () => {
+    cy.get('.categorize__button__container')
+  })
+
+  it('should click surname category button and have active class', () => {
+    cy.get('.categorize__button__container')
+    cy.get('.group__button').first().click().should('have.class', 'group__button--active')
+  })
+
+  it('should click surname category button and other buttons should not have active class', () => {
+    cy.get('.categorize__button__container')
+    cy.get('.group__button').first().click().should('have.class', 'group__button--active')
+    cy.get('.group__button').last().should('have.class', 'group__button')
+    cy.get('.group__button').last().should('not.have.class', 'group__button--active')
+  })
 })
